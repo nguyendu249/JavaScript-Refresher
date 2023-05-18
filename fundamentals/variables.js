f346745995ceb8262552f60d9e4b8a87eba6a9ac
unknown <nguyendu240901@gmail.com>
2023-05-18 23:58:17 +0700

    fundamentals

---
+++ fundamentals/variables.js
@@ -0,0 +1,17 @@
+let message = 'Hello';
+message = 100;
+console.log(message); // 100
+
+let age;
+console.log(age); // undefined
+
+console.log(counter); // counter is not defined
+
+// // Constant
+// const workday = 5;
+// // sau khi đã xác định const, bạn không thể thay đổi giá trị của nó
+// const warkday = 3;
+// console.log(workday); // Uncaught TypeError: Assignment to constant variable.
+
+const PI = 3.14; //not changed value
+// PI = 3.15 -> Assignment to constant variable
